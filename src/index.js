7cf6882bd27671599f9d452f84c4315cffefeaa8
davidquero <32507239+davidquero@users.noreply.github.com>
2019-01-10 17:44:44 +0100

    Form submit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
-import { HERO } from "./config";
 import { getMarvelDataFor } from "./api";
 
-getMarvelDataFor(HERO);
+let form = document.querySelector("form");
+form.addEventListener("submit", event => {
+  let hero = form.elements.search.value;
+
+  if (hero != null && hero !== "") {
+    getMarvelDataFor(hero);
+  }
+
+  event.preventDefault();
+});
