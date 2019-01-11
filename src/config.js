444b41a15a1e076bccd3d7c066c7de98b2754666
davidquero <32507239+davidquero@users.noreply.github.com>
2019-01-11 23:10:37 +0100

    Final

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 export const BASE_URL = "https://gateway.marvel.com:443/v1/public";
 export const API_PUBLIC_KEY = "4f4a67b4f618fa5387aaf0ab07c500dd";
-//export const API_PUBLIC_KEY = "89e6e25f0050fc2635cf578b06605a01";
 export const MIN_HEROES = 3;
